6ce43e1ccc7175fb06e85c7243ab0a17754c8917
ssrtvhelp <145517817+ssrtvhelp@users.noreply.github.com>
2026-01-26 12:55:37 +0800

    Add files via upload

--- py/腾讯视频.py
+++ py/腾讯视频.py
@@ -259,10 +259,7 @@ class Spider(Spider):
         return {'list': vlist, 'page': pg}
 
     def playerContent(self, flag, id, vipFlags):
-        ids = id.split('@')
-        url = f"{self.host}/x/cover/{ids[0]}/{ids[1]}.html"
-        parse_url = f""
-        return {'parse': 1, 'url': parse_url, 'header': ''}
+        return  {'jx':1,'parse': 1, 'url': f"{self.ihost}/video?vid={id}", 'header': ''}
         
     def localProxy(self, param):
         pass
